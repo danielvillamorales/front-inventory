b08ba39be3cf1437aa041561d023039c0493c508
danielvillamorales <daniel.villa@utp.edu.co>
2022-10-06 16:12:48 -0500

    cerrar sesion

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-sidenav',
@@ -9,17 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class SidenavComponent implements OnInit {
 
   mobileQuery: MediaQueryList;
+  username: any;
   menuNav =[
     {name:"Home", route: "home", icon: "home"},
     {name:"Categorias", route: "category", icon: "category"},
     {name:"Productos", route: "product", icon: "production_quantity_limits"}
   ]
 
-  constructor(media: MediaMatcher) {
+  constructor(media: MediaMatcher, private keyLoackService : KeycloakService) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)')
   }
 
   ngOnInit(): void {
+  this.username = this.keyLoackService.getUsername();
+  }
+
+
+  logout(){
+    this.keyLoackService.logout();
   }
 
 }
